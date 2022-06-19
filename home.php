96bce4bba09ee178b4bdcf9ea7cfea5ead07d5ad
Vít Ružička <74555761+vito104@users.noreply.github.com>
2022-06-19 13:45:55 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,20 @@
+<div class="center">
+      <h1>Prosím, přihlašte se</h1>
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Uživatelské jméno(email)</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Heslo</label>
+        </div>
+        <div class="pass">Zapomenuté heslo?</div>
+        <input type="submit" value="Přihlásit se">
+        <div class="signup_link">
+          Nový uživatel? <a href="index.php?page=userreg">Žádost o registraci</a>
+        </div>
+      </form>
+    </div>
